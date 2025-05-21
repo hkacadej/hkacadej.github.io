016cf4c2af66dc08745b51a0ea27df1f52f5b310
henri.kacadej <henri.kacadej@facilization.com>
2025-05-21 16:35:54 +0200

    Adding Sources

---
+++ sript.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 70,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
